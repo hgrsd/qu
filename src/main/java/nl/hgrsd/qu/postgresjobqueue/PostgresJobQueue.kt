927e2c968c815c1fdafed188da8f6c4a58d644a1
hgrsd <daniel@hgrsd.nl>
2022-08-25 23:53:53 +0100

    scheduled and fetch jobs in PG queue

--- src/main/java/nl/hgrsd/qu/postgresjobqueue/PostgresJobQueue.kt
+++ src/main/java/nl/hgrsd/qu/postgresjobqueue/PostgresJobQueue.kt
@@ -1,14 +1,49 @@
 package nl.hgrsd.qu.postgresjobqueue
 
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
 import nl.hgrsd.qu.JobQueue.Job
 import nl.hgrsd.qu.JobQueue.JobQueue
+import nl.hgrsd.qu.JobQueue.JobStatus
+import org.postgresql.util.PGobject
+import java.lang.Error
+import java.sql.Connection
+import java.sql.Date
+import java.sql.ResultSet
 import java.time.Instant
 import java.util.UUID
 import java.util.Optional
 
-class PostgresJobQueue<T>: JobQueue<T> {
+class PostgresJobQueue<T>(private val conn: Connection, private val serializer: KSerializer<T>) : JobQueue<T> {
+    private fun deserializeJob(rs: ResultSet): Job<T> {
+        val obj: PGobject = rs.getObject("payload") as PGobject;
+        val payload = Json.decodeFromString(serializer, obj.value.orEmpty());
+        val status: JobStatus = when (rs.getString("status")) {
+            "QUEUED" -> JobStatus.QUEUED;
+            "IN_PROGRESS" -> JobStatus.IN_PROGRESS;
+            "FAILED" -> JobStatus.FAILED;
+            "COMPLETED" -> JobStatus.COMPLETED;
+            else -> {
+                throw Error("Unexpected status");
+            }
+        }
+        val scheduledFor = Optional.ofNullable(rs.getDate("scheduled_for")).map { it.toInstant() };
+        return Job(payload, rs.getObject("job_id") as UUID, status, scheduledFor)
+    }
     override fun scheduleJob(job: Job<T>) {
-        TODO("Not yet implemented")
+        val s = conn.prepareStatement(
+            "INSERT INTO qu (job_id, scheduled_for, status, payload)" +
+                    "VALUES (?, ?, ?, ?);"
+        );
+        s.setObject(1, job.id());
+        job.scheduledFor()
+            .ifPresentOrElse({ s.setDate(2, Date(it.epochSecond)) }, { s.setNull(2, java.sql.Types.NULL) });
+        s.setString(3, job.status.toString());
+        val payload = PGobject()
+        payload.type = "jsonb"
+        payload.value = Json.encodeToString(serializer, job.data())
+        s.setObject(4, payload);
+        s.execute();
     }
 
     override fun deleteJob(id: UUID) {
@@ -28,7 +63,14 @@ class PostgresJobQueue<T>: JobQueue<T> {
     }
 
     override fun getJob(id: UUID): Optional<Job<T>> {
-        TODO("Not yet implemented")
+        val s = conn.prepareStatement("SELECT * FROM qu WHERE job_id = ?");
+        s.setObject(1, id);
+
+        val rs = s.executeQuery();
+        if (!rs.next()) {
+            return Optional.empty()
+        }
+        return Optional.of(deserializeJob(rs))
     }
 
 }
--- src/test/java/nl/hgrsd/qu/postgresjobqueue/PostgresJobQueueTest.kt
+++ src/test/java/nl/hgrsd/qu/postgresjobqueue/PostgresJobQueueTest.kt
@@ -1,7 +1,35 @@
 package nl.hgrsd.qu.postgresjobqueue
 
-import org.junit.jupiter.api.Assertions.*
+import kotlinx.serialization.Serializable
+import nl.hgrsd.qu.JobQueue.Job
+import nl.hgrsd.qu.JobQueue.JobStatus
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.sql.Connection
+import java.sql.DriverManager
+import java.util.*
 
+fun getConnection(): Connection {
+    val dburl = System.getenv("QU_DBURL");
+    val pw = System.getenv("QU_DBPASSWORD");
+    val un = System.getenv("QU_DBUSERNAME");
+    return DriverManager.getConnection("jdbc:${dburl}", un, pw);
+}
+
+@Serializable
+data class TestPayload(val content: String)
 internal class PostgresJobQueueTest {
+    @Test
+    fun postgresJobQueue_canScheduleJob() {
+        val conn = getConnection();
+        val qu = PostgresJobQueue(conn, TestPayload.serializer());
+        val job = Job(TestPayload("hi"), UUID.randomUUID(), JobStatus.QUEUED, Optional.empty());
+        qu.scheduleJob(job);
+        val scheduled = qu.getJob(job.id()).get()
+        Assertions.assertEquals(scheduled.id(), job.id())
+        Assertions.assertEquals(scheduled.data(), job.data())
+        Assertions.assertEquals(scheduled.scheduledFor(), job.scheduledFor())
+        Assertions.assertEquals(scheduled.status, job.status)
+    }
 
 }
