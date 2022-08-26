8dcac769eea6921cc8975ae4ffe29d3e4f255200
hgrsd <daniel@hgrsd.nl>
2022-08-26 12:15:48 +0100

    implement postgres job queue

--- src/main/java/nl/hgrsd/qu/postgresjobqueue/PostgresJobQueue.kt
+++ src/main/java/nl/hgrsd/qu/postgresjobqueue/PostgresJobQueue.kt
@@ -8,68 +8,101 @@ import nl.hgrsd.qu.JobQueue.JobStatus
 import org.postgresql.util.PGobject
 import java.lang.Error
 import java.sql.Connection
-import java.sql.Date
 import java.sql.ResultSet
+import java.sql.Timestamp
 import java.time.Instant
 import java.util.UUID
 import java.util.Optional
 
 class PostgresJobQueue<T>(private val conn: Connection, private val serializer: KSerializer<T>) : JobQueue<T> {
     private fun deserializeJob(rs: ResultSet): Job<T> {
-        val obj: PGobject = rs.getObject("payload") as PGobject;
-        val payload = Json.decodeFromString(serializer, obj.value.orEmpty());
+        val obj: PGobject = rs.getObject("payload") as PGobject
+        val payload = Json.decodeFromString(serializer, obj.value.orEmpty())
         val status: JobStatus = when (rs.getString("status")) {
-            "QUEUED" -> JobStatus.QUEUED;
-            "IN_PROGRESS" -> JobStatus.IN_PROGRESS;
-            "FAILED" -> JobStatus.FAILED;
-            "COMPLETED" -> JobStatus.COMPLETED;
+            "QUEUED" -> JobStatus.QUEUED
+            "IN_PROGRESS" -> JobStatus.IN_PROGRESS
+            "FAILED" -> JobStatus.FAILED
+            "COMPLETED" -> JobStatus.COMPLETED
             else -> {
-                throw Error("Unexpected status");
+                throw Error("Unexpected status")
             }
         }
-        val scheduledFor = Optional.ofNullable(rs.getDate("scheduled_for")).map { it.toInstant() };
+        val scheduledFor = Optional.ofNullable(rs.getTimestamp("scheduled_for")).map { it.toInstant() }
         return Job(payload, rs.getObject("job_id") as UUID, status, scheduledFor)
     }
 
     override fun scheduleJob(job: Job<T>) {
         val s = conn.prepareStatement(
-            "INSERT INTO qu (job_id, scheduled_for, status, payload)" +
-                    "VALUES (?, ?, ?, ?);"
-        );
-        s.setObject(1, job.id());
+            """
+                INSERT INTO qu 
+                (job_id, scheduled_for, status, payload)
+                VALUES (?, ?, ?, ?)
+                """
+        )
+        s.setObject(1, job.id())
         job.scheduledFor()
-            .ifPresentOrElse({ s.setDate(2, Date(it.epochSecond)) }, { s.setNull(2, java.sql.Types.NULL) });
-        s.setString(3, job.status.toString());
+            .ifPresentOrElse({ s.setTimestamp(2, Timestamp.from(it)) }, { s.setNull(2, java.sql.Types.NULL) })
+        s.setString(3, job.status.toString())
         val payload = PGobject()
         payload.type = "jsonb"
         payload.value = Json.encodeToString(serializer, job.data())
-        s.setObject(4, payload);
-        s.execute();
+        s.setObject(4, payload)
+        s.execute()
     }
 
     override fun deleteJob(id: UUID) {
-        val s = conn.prepareStatement("DELETE FROM qu WHERE job_id = ?");
-        s.setObject(1, id);
-        s.execute();
+        val s = conn.prepareStatement("DELETE FROM qu WHERE job_id = ?")
+        s.setObject(1, id)
+        s.execute()
     }
 
     override fun pullJobs(cutOff: Instant, maxJobs: Int): MutableList<Job<T>> {
-        TODO("Not yet implemented")
+        val s = conn.prepareStatement(
+            """
+                UPDATE qu SET STATUS = ?
+                WHERE internal_id IN (
+                    SELECT internal_id 
+                    FROM qu 
+                    WHERE STATUS = ?
+                    AND (scheduled_for IS NULL OR scheduled_for <= ?)
+                    ORDER BY internal_id
+                    FOR UPDATE SKIP LOCKED
+                    LIMIT ?
+                ) RETURNING *;
+                """
+        )
+        s.setString(1, JobStatus.IN_PROGRESS.toString())
+        s.setString(2, JobStatus.QUEUED.toString())
+        s.setTimestamp(3, Timestamp.from(cutOff))
+        s.setInt(4, maxJobs)
+
+        val jobs = mutableListOf<Job<T>>()
+        val rs = s.executeQuery()
+        while (rs.next()) {
+            jobs.add(deserializeJob(rs))
+        }
+        return jobs
     }
 
     override fun completeJob(id: UUID) {
-        TODO("Not yet implemented")
+        val s = conn.prepareStatement("UPDATE qu SET status = ? WHERE job_id = ?;")
+        s.setString(1, JobStatus.COMPLETED.toString())
+        s.setObject(2, id)
+        s.execute()
     }
 
     override fun markJobAsFailed(id: UUID) {
-        TODO("Not yet implemented")
+        val s = conn.prepareStatement("UPDATE qu SET status = ? WHERE job_id = ?;")
+        s.setString(1, JobStatus.FAILED.toString())
+        s.setObject(2, id)
+        s.execute()
     }
 
     override fun getJob(id: UUID): Optional<Job<T>> {
-        val s = conn.prepareStatement("SELECT * FROM qu WHERE job_id = ?");
-        s.setObject(1, id);
+        val s = conn.prepareStatement("SELECT * FROM qu WHERE job_id = ?")
+        s.setObject(1, id)
 
-        val rs = s.executeQuery();
+        val rs = s.executeQuery()
         if (!rs.next()) {
             return Optional.empty()
         }
--- src/test/java/nl/hgrsd/qu/postgresjobqueue/PostgresJobQueueTest.kt
+++ src/test/java/nl/hgrsd/qu/postgresjobqueue/PostgresJobQueueTest.kt
@@ -4,28 +4,37 @@ import kotlinx.serialization.Serializable
 import nl.hgrsd.qu.JobQueue.Job
 import nl.hgrsd.qu.JobQueue.JobStatus
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.sql.Connection
 import java.sql.DriverManager
+import java.time.Instant
 import java.util.*
 
 fun getConnection(): Connection {
-    val dburl = System.getenv("QU_DBURL");
-    val pw = System.getenv("QU_DBPASSWORD");
-    val un = System.getenv("QU_DBUSERNAME");
-    return DriverManager.getConnection("jdbc:${dburl}", un, pw);
+    val dburl = System.getenv("QU_DBURL")
+    val pw = System.getenv("QU_DBPASSWORD")
+    val un = System.getenv("QU_DBUSERNAME")
+    return DriverManager.getConnection("jdbc:${dburl}", un, pw)
 }
 
 @Serializable
 data class TestPayload(val content: String)
 
 internal class PostgresJobQueueTest {
+    @BeforeEach
+    fun clear() {
+        val conn = getConnection()
+        val st = conn.createStatement()
+        st.execute("DELETE FROM qu")
+    }
+
     @Test
     fun postgresJobQueue_canScheduleJob() {
-        val conn = getConnection();
-        val qu = PostgresJobQueue(conn, TestPayload.serializer());
-        val job = Job(TestPayload("hi"), UUID.randomUUID(), JobStatus.QUEUED, Optional.empty());
-        qu.scheduleJob(job);
+        val conn = getConnection()
+        val qu = PostgresJobQueue(conn, TestPayload.serializer())
+        val job = Job(TestPayload("hi"), UUID.randomUUID(), JobStatus.QUEUED, Optional.empty())
+        qu.scheduleJob(job)
         val scheduled = qu.getJob(job.id()).get()
         Assertions.assertEquals(scheduled.id(), job.id())
         Assertions.assertEquals(scheduled.data(), job.data())
@@ -35,21 +44,206 @@ internal class PostgresJobQueueTest {
 
     @Test
     fun postgresJobQueue_returnsEmpty() {
-        val conn = getConnection();
-        val qu = PostgresJobQueue(conn, TestPayload.serializer());
-        val empty = qu.getJob(UUID.randomUUID());
-        Assertions.assertTrue(empty.isEmpty);
+        val conn = getConnection()
+        val qu = PostgresJobQueue(conn, TestPayload.serializer())
+        val empty = qu.getJob(UUID.randomUUID())
+        Assertions.assertTrue(empty.isEmpty)
     }
 
     @Test
     fun postgresJobQueue_deletesJob() {
-        val conn = getConnection();
-        val qu = PostgresJobQueue(conn, TestPayload.serializer());
-        val job = Job(TestPayload("hi"), UUID.randomUUID(), JobStatus.QUEUED, Optional.empty());
-        qu.scheduleJob(job);
+        val conn = getConnection()
+        val qu = PostgresJobQueue(conn, TestPayload.serializer())
+        val job = Job(TestPayload("hi"), UUID.randomUUID(), JobStatus.QUEUED, Optional.empty())
+        qu.scheduleJob(job)
         qu.deleteJob(job.id())
-        val scheduled = qu.getJob(job.id());
+        val scheduled = qu.getJob(job.id())
         Assertions.assertTrue(scheduled.isEmpty)
+    }
+
+    @Test
+    fun postgresJobQueue_pullsJobsInOrder() {
+        val conn = getConnection()
+        val qu = PostgresJobQueue(conn, TestPayload.serializer())
+        val j0 = Job(TestPayload("test"), UUID.randomUUID(), JobStatus.QUEUED, Optional.empty())
+        val j1 = Job(TestPayload("test1"), UUID.randomUUID(), JobStatus.QUEUED, Optional.empty())
+        val j2 = Job(TestPayload("test2"), UUID.randomUUID(), JobStatus.QUEUED, Optional.empty())
+        qu.scheduleJob(j0)
+        qu.scheduleJob(j1)
+        qu.scheduleJob(j2)
+
+        val jobs = qu.pullJobs(Instant.now().plusMillis(1000), 10)
+
+        Assertions.assertEquals(3, jobs.size)
+        Assertions.assertEquals(
+            Job(
+                j0.data(),
+                j0.id(),
+                JobStatus.IN_PROGRESS,
+                j0.scheduledFor()
+            ), jobs[0]
+        )
+        Assertions.assertEquals(
+            Job(
+                j1.data(),
+                j1.id(),
+                JobStatus.IN_PROGRESS,
+                j1.scheduledFor()
+            ), jobs[1]
+        )
+        Assertions.assertEquals(
+            Job(
+                j2.data(),
+                j2.id(),
+                JobStatus.IN_PROGRESS,
+                j2.scheduledFor()
+            ), jobs[2]
+        )
+    }
+
+    @Test
+    fun postgresJobQueue_returnsJobsUsingCutoff() {
+        val conn = getConnection()
+        val qu = PostgresJobQueue(conn, TestPayload.serializer())
+        val j0 = Job(
+            TestPayload("should not return"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.of(Instant.parse("2022-01-01T00:00:00.000Z"))
+        )
+        val j1 = Job(
+            TestPayload("should return"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.of(Instant.parse("2021-12-31T23:59:59.999Z"))
+        )
+        qu.scheduleJob(j0)
+        qu.scheduleJob(j1)
+
+        val jobs = qu.pullJobs(Instant.parse("2021-12-31T23:59:59.999Z"), 10)
+
+        Assertions.assertEquals(1, jobs.size)
+        Assertions.assertEquals(
+            Job(
+                j1.data(),
+                j1.id(),
+                JobStatus.IN_PROGRESS,
+                j1.scheduledFor()
+            ), jobs[0]
+        )
+    }
+
+    @Test
+    fun postgresJobQueue_respectsMax() {
+        val conn = getConnection()
+        val qu = PostgresJobQueue(conn, TestPayload.serializer())
+        val j0 = Job(
+            TestPayload("test"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.empty()
+        )
+        val j1 = Job(
+            TestPayload("test"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.empty()
+        )
+        val j2 = Job(
+            TestPayload("test"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.empty()
+        )
+        qu.scheduleJob(j0)
+        qu.scheduleJob(j1)
+        qu.scheduleJob(j2)
+
+        val jobs = qu.pullJobs(Instant.now(), 2)
+
+        Assertions.assertEquals(2, jobs.size)
+        Assertions.assertEquals(
+            Job(
+                j0.data(),
+                j0.id(),
+                JobStatus.IN_PROGRESS,
+                j0.scheduledFor()
+            ), jobs[0]
+        )
+        Assertions.assertEquals(
+            Job(
+                j1.data(),
+                j1.id(),
+                JobStatus.IN_PROGRESS,
+                j1.scheduledFor()
+            ), jobs[1]
+        )
+    }
+
+    @Test
+    fun postgresJobQueue_doesNotReturnInProgressJobs() {
+        val conn = getConnection()
+        val qu = PostgresJobQueue(conn, TestPayload.serializer())
+        val j0 = Job(
+            TestPayload("test"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.empty()
+        )
+        val j1 = Job(
+            TestPayload("test"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.empty()
+        )
+        val j2 = Job(
+            TestPayload("test"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.empty()
+        )
+        qu.scheduleJob(j0)
+        qu.scheduleJob(j1)
+        qu.scheduleJob(j2)
+
+        val jobs1 = qu.pullJobs(Instant.now(), 1)
+
+        Assertions.assertEquals(1, jobs1.size)
+
+        val jobs2 = qu.pullJobs(Instant.now(), 10)
+        Assertions.assertEquals(2, jobs2.size)
+    }
+
+    @Test
+    fun postgresJobQueue_marksAsCompleted() {
+        val conn = getConnection()
+        val qu = PostgresJobQueue(conn, TestPayload.serializer())
+        val j0 = Job(
+            TestPayload("test"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.empty()
+        )
+        qu.scheduleJob(j0)
+        qu.completeJob(j0.id());
+        val job = qu.getJob(j0.id()).get()
+        Assertions.assertEquals(JobStatus.COMPLETED, job.status)
+    }
+
+    @Test
+    fun postgresJobQueue_marksAsFailed() {
+        val conn = getConnection()
+        val qu = PostgresJobQueue(conn, TestPayload.serializer())
+        val j0 = Job(
+            TestPayload("test"),
+            UUID.randomUUID(),
+            JobStatus.QUEUED,
+            Optional.empty()
+        )
+        qu.scheduleJob(j0)
+        qu.markJobAsFailed(j0.id());
+        val job = qu.getJob(j0.id()).get()
+        Assertions.assertEquals(JobStatus.FAILED, job.status)
     }
 
 }
